809ebfced9772ee042462002bda1f58e269a86de
Bernie <55376037+BernieGroenewald@users.noreply.github.com>
2019-11-18 08:41:13 +0200

    Add files via upload

---
+++ C# Code/SentryAdmin/AddUser.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace SentryAdmin
+{
+    partial class AddUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddUser));
+            this.label13 = new System.Windows.Forms.Label();
+            this.cbRole = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tEmail = new System.Windows.Forms.TextBox();
+            this.tName = new System.Windows.Forms.TextBox();
+            this.tNetworkName = new System.Windows.Forms.TextBox();
+            this.cmdSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(105, 258);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(29, 13);
+            this.label13.TabIndex = 84;
+            this.label13.Text = "Role";
+            // 
+            // cbRole
+            // 
+            this.cbRole.FormattingEnabled = true;
+            this.cbRole.Location = new System.Drawing.Point(140, 255);
+            this.cbRole.Name = "cbRole";
+            this.cbRole.Size = new System.Drawing.Size(278, 21);
+            this.cbRole.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 78;
+            this.label6.Text = "Network User ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 82;
+            this.label3.Text = "Email Address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 81;
+            this.label1.Text = "Name";
+            // 
+            // tEmail
+            // 
+            this.tEmail.Location = new System.Drawing.Point(140, 229);
+            this.tEmail.Name = "tEmail";
+            this.tEmail.Size = new System.Drawing.Size(278, 20);
+            this.tEmail.TabIndex = 2;
+            // 
+            // tName
+            // 
+            this.tName.Location = new System.Drawing.Point(140, 203);
+            this.tName.Name = "tName";
+            this.tName.Size = new System.Drawing.Size(278, 20);
+            this.tName.TabIndex = 1;
+            // 
+            // tNetworkName
+            // 
+            this.tNetworkName.Location = new System.Drawing.Point(140, 177);
+            this.tNetworkName.Name = "tNetworkName";
+            this.tNetworkName.Size = new System.Drawing.Size(278, 20);
+            this.tNetworkName.TabIndex = 0;
+            // 
+            // cmdSave
+            // 
+            this.cmdSave.Image = ((System.Drawing.Image)(resources.GetObject("cmdSave.Image")));
+            this.cmdSave.Location = new System.Drawing.Point(343, 314);
+            this.cmdSave.Name = "cmdSave";
+            this.cmdSave.Size = new System.Drawing.Size(75, 36);
+            this.cmdSave.TabIndex = 4;
+            this.cmdSave.Text = "Save";
+            this.cmdSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.cmdSave.UseVisualStyleBackColor = true;
+            this.cmdSave.Click += new System.EventHandler(this.cmdSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(249, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(169, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 87;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 18);
+            this.label2.TabIndex = 88;
+            this.label2.Text = "Your user was not found.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(360, 18);
+            this.label4.TabIndex = 89;
+            this.label4.Text = "Please complete your information below and click \"Save\"";
+            // 
+            // AddUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 368);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmdSave);
+            this.Controls.Add(this.tNetworkName);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.cbRole);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tEmail);
+            this.Controls.Add(this.tName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New User";
+            this.Load += new System.EventHandler(this.AddUser_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ComboBox cbRole;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tEmail;
+        private System.Windows.Forms.TextBox tName;
+        private System.Windows.Forms.TextBox tNetworkName;
+        private System.Windows.Forms.Button cmdSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ C# Code/SentryAdmin/Register.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Windows.Forms;
+using SentryDataStuff;
+
+namespace SentryAdmin
+{
+    public partial class Register : Form
+    {
+        bool IsValidKey = true;
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+            cmdRegister.Enabled = false;
+            tReg1.Focus();
+        }
+
+        private void tReg1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(tReg1.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("Please enter a numeric value.");
+                    tReg1.Text = tReg1.Text.Remove(tReg1.Text.Length - 1);
+                }
+
+                if ((tReg1.Text.Length == 4) && (tReg2.Text.Length == 4) && (tReg3.Text.Length == 4) && (tCompanyName.Text.Trim() != ""))
+                {
+                    cmdRegister.Enabled = true;
+                }
+                else
+                {
+                    cmdRegister.Enabled = false;
+                }
+
+                if (tReg1.Text.Length == 4)
+                {
+                    tReg2.Focus();
+                }
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void tReg2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(tReg2.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("Please enter a numeric value.");
+                    tReg2.Text = tReg2.Text.Remove(tReg2.Text.Length - 1);
+                }
+
+                if ((tReg1.Text.Length == 4) && (tReg2.Text.Length == 4) && (tReg3.Text.Length == 4) && (tCompanyName.Text.Trim() != ""))
+                {
+                    cmdRegister.Enabled = true;
+                }
+                else
+                {
+                    cmdRegister.Enabled = false;
+                }
+
+                if (tReg2.Text.Length == 4)
+                {
+                    tReg3.Focus();
+                }
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void tReg3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(tReg3.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("Please enter a numeric value.");
+                    tReg3.Text = tReg3.Text.Remove(tReg3.Text.Length - 1);
+                }
+
+                if ((tReg1.Text.Length == 4) && (tReg2.Text.Length == 4) && (tReg3.Text.Length == 4) && (tCompanyName.Text.Trim() != ""))
+                {
+                    cmdRegister.Enabled = true;
+                }
+                else
+                {
+                    cmdRegister.Enabled = false;
+                }
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void cmdRegister_Click(object sender, EventArgs e)
+        {
+            string FirstReg = string.Empty;
+            string Part1 = string.Empty;
+            string Part2 = string.Empty;
+            string Part3 = string.Empty;
+            int KeyPart1 = 0;
+            int KeyPart2 = 0;
+            int KeyPart3 = 0;
+            int DaysNow = 0;
+            DateTime Date2000;
+            int NumberOfUsers = 0;
+
+            if ((tReg1.Text.Length != 4) || (tReg2.Text.Length != 4) || (tReg3.Text.Length != 4) || (tCompanyName.Text.Trim() == ""))
+            {
+                if (tCompanyName.Text.Trim() == "")
+                {
+                    MessageBox.Show("Please enter the name of your organization.", "Register", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                MessageBox.Show("The registration code is invalid, please enter a valid code.", "Register", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            IsValidKey = true;
+
+            try
+            {
+                using (DataStuff sn = new DataStuff())
+                {
+                    FirstReg = sn.GetSystemSetting("Key1");
+                    
+                    if (FirstReg.Trim() == "")
+                    {
+                        FirstReg = DateTime.Now.ToString("dd MMM yyyy");
+
+                        sn.SaveSystemSetting("Key1", FirstReg);
+                    }
+                }
+            }
+
+            catch
+            {
+
+            }
+            
+            try
+            {
+                Part1 = tReg1.Text;
+                Part2 = tReg2.Text;
+                Part3 = tReg3.Text;
+
+                if (int.TryParse(Part1, out KeyPart1))
+                {
+                    Date2000 = Convert.ToDateTime("22 Aug 2016");
+                    DaysNow = (DateTime.Now - Date2000).Days;
+
+                    if (!((KeyPart1 - 1009) >= DaysNow))
+                    {
+                        IsValidKey = false;
+                    }
+                }
+                else
+                {
+                    IsValidKey = false;
+                }
+
+                if (int.TryParse(Part2, out KeyPart2))
+                {
+                    if (KeyPart2 > 4831)
+                    {
+                        NumberOfUsers = 0;
+                        IsValidKey = false;
+                    }
+                    else
+                    {
+                        NumberOfUsers = 4831 - KeyPart2;
+                    }
+                }
+                else
+                {
+                    IsValidKey = false;
+                }
+
+                if (int.TryParse(Part3, out KeyPart3))
+                {
+                    if (KeyPart3 % 22 != 0)
+                    {
+                        IsValidKey = false;
+                    }
+                }
+                else
+                {
+                    IsValidKey = false;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                IsValidKey = false;
+            }
+
+            if (IsValidKey)
+            {
+                try
+                {
+                    using (DataStuff sn = new DataStuff())
+                    {
+                        sn.SaveSystemSetting("Key2", tReg1.Text.Trim() + tReg2.Text.Trim() + tReg3.Text.Trim());
+                        sn.SaveSystemSetting("Key3", tCompanyName.Text);
+                        sn.SaveSystemSetting("Key4", tReg1.Text.Trim() + tReg2.Text.Trim() + tReg3.Text.Trim());
+                    }
+
+                    MessageBox.Show("Thank you for registering your version of SQLSure.", "Register", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("The registration code is invalid, please enter a valid code.", "Register", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+        }
+
+        private void tCompanyName_TextChanged(object sender, EventArgs e)
+        {
+            if ((tReg1.Text.Length == 4) && (tReg2.Text.Length == 4) && (tReg3.Text.Length == 4) && (tCompanyName.Text.Trim() != ""))
+            {
+                cmdRegister.Enabled = true;
+            }
+            else
+            {
+                cmdRegister.Enabled = false;
+            }
+        }
+    }
+}
